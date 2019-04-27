3c4dcf1eaa03287f4d9305bbdb8af0c420d11146
Harish <gharish1997@gmail.com>
2019-04-27 17:30:19 +0530

    calc task

--- calc/src/App.js
+++ calc/src/App.js
@@ -37,8 +37,11 @@ export class App extends Component {
     //   result: eval(this.state.result)
     // });
     this.setState((prevStates) => {
-      return {
-        result: eval(prevStates.result)
+      try {return {
+        result: eval(prevStates.result)  }
+      }
+      catch (e){
+        alert("Please Enter Only Numbers")
       }
     })
   }
@@ -55,11 +58,24 @@ export class App extends Component {
     });
   }
 
+  handleKeyDown = (e) => {
+    let code = e.keyCode || e.which;
+    if (code === 13) {
+      this.solve();
+    }
+  }
+
+  // a = () => {
+  //   document.querySelector(window).addEventListner("keypress", () => {
+  //     this.solve();
+  //   });
+  // }
+
   render() {
     return (
       <div className="App">
-        <Result result={this.state.result} inputType={this.inputType}/>
-        <Keypad  buttonClick={this.buttonClick} clearEverything={this.clearEverything} solve={this.solve} clearFunc={this.clearFunc}/>
+        <Result result={this.state.result} inputType={this.inputType} keyHandler = {this.handleKeyDown}/>
+        <Keypad  buttonClick={this.buttonClick} clearEverything={this.clearEverything} solve={this.solve} clearFunc={this.clearFunc} />
       </div>
     )
   }
--- calc/src/components/Displayresult.js
+++ calc/src/components/Displayresult.js
@@ -5,10 +5,12 @@ import './comp.css';
 export class Displayresult extends Component {
     
   render() {
+
+
     // $('#input').inputfit();
     return (
       <div className="buttonWrapper">
-        <input type="text" id="input" onChange = {this.props.inputType} value={this.props.result} className="resultDisplay"></input>
+        <input type="text"  id="input" onChange = {this.props.inputType} onKeyPress={this.props.keyHandler} value={this.props.result} className="resultDisplay"></input>
       </div>
     )
   }
